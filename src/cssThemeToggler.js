deb452938e3b5a066f1b61e62978264969670bc2
chonsukp <chonsukp@gmail.com>
2024-08-31 10:32:05 +1000

    feat: saving and loading theme working

--- src/cssThemeToggler.js
+++ src/cssThemeToggler.js
@@ -1,36 +1,69 @@
-console.log("CSS Theme Toggler loaded and running");
+console.log("CSS Theme toggler loaded and running!");
+
 
 let themes = [
-    {
-        name: "dark",
-        properties: {
-            backgroundColour: "darkgrey",
-            fontColour: "white",
-            "theme-100": "#4641d1"
-        }
-    },
-    {
-        name: "light",
-        properties: {
-            backgroundColour: "#87E0E0",
-            fontColour: "black"
-            ,
-            "theme-100": "#87E0E0"
-        }
-    }
+	{
+		name: "dark",
+		properties: {
+			backgroundColour: "darkgrey",
+			fontColour: "white",
+			"theme-100": "#4641d1"
+		}
+	},
+	{
+		name: "light",
+		properties: {
+			backgroundColour: "#87E0E0",
+			fontColour: "black"
+			,
+			"theme-100": "#87E0E0"
+		}
+	}
 ];
 
 // Read theme name stored in local storage 
 // and update CSS variables based on that name 
 function getChosenTheme(){
-
+	let foundTheme = localStorage.getItem("theme");
+	console.log(foundTheme);
+	return foundTheme;
 }
 
 // Set theme name to local storage 
 // and update CSS variables based on that name 
 function setChosenTheme(newThemeName){
-
+	localStorage.setItem("theme", newThemeName);
 }
+
+if (getChosenTheme() == null){
+	const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+	if (darkThemeMq.matches) {
+	  // Theme set to dark.
+	  setChosenTheme("dark");
+	  console.log("No theme found, applied the dark theme");
+	} else {
+	  // Theme set to light.
+	  setChosenTheme("light");
+	  console.log("No theme found, applied the light theme");
+
+	}
+}
+
+function toggleTheme(){
+	// if ("dark" == "dark"){
+	// if ("light" == "dark"){
+	if (getChosenTheme() == "dark"){
+		// set it to light 
+		setChosenTheme("light");
+	} else {
+		// set it to dark 
+		setChosenTheme("dark");
+	}
+}
+
+let themeToggleButton = document.getElementById("themeToggle");
+themeToggleButton.onclick = toggleTheme;
+// themeToggleButton.addEventListener("click", toggleTheme);
 
 // Loop through properties key in chosen theme object 
 // and apply those properties to CSS 
@@ -38,14 +71,19 @@ function updateCssVariables(){
 
 }
 
-let rootElement = document.querySelector(":root")
 
-function getVariblesFromCSS(){
-    console.log(rootElement);
+let rootElement = document.querySelector(":root");
 
-    let rootStyles = getComputedStyle(rootElement);
+function getVariablesFromCSS(){
+	console.log(rootElement);
 
-    console.log(rootStyles.getPropertyValue("--backgroundColour"));
+	// console.log(document.documentElement.style.getPropertyValue("--backgroundColour"));
+
+	let rootStyles = getComputedStyle(rootElement);
+
+	console.log(rootStyles.getPropertyValue("--backgroundColour"));
+
 }
 
-getVariblesFromCSS()
+getVariablesFromCSS();
+
